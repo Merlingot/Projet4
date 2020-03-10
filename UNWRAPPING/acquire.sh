cb0b7b052e92f47f06e1ae2b4ebfee141c44e03b
jcaudet <imflyingtoast@hotmail.com>
2020-03-10 16:38:56 -0400

    acquire.sh

--- UNWRAPPING/acquire.sh
+++ UNWRAPPING/acquire.sh
@@ -13,30 +13,59 @@
 ## Function definition
 
 save_Thrash(){
-	cd archive
-	local found = false
-	local i=$0
-
-	while["$found"=false];
-	do
-		if [ ! -d "scan_$ ]
-
+	
+	local found="false"
+	local i=0
+	while [ $found = "false" ]
+	do	
+		if [ ! -d "./archive/scan_$1_3channels_$i" ]; then
+			mv "./scan_$1_3channels" "./archive/scan_$1_3channels_$i"
+			found="true"
+		else
+			i=$(($i+1))
+		fi
+	done
 }
 
-# Check if build/ dir exists.
-if [ ! -d scan_AV_3channels ]; then
-	mkdir scan_AV_3channels
-else
-	cp -r scan_AV_3channels
-	mkdir  scan_AV_3channels
-fi
 
-if [ ! -d scan_PG_3channels ]; then
-	mkdir scan_PG_3channels
-else
-	rm -rf scan_PG_3channels
-	mkdir scan_PG_3channels
-fi
+manage_folders(){
+	if [ ! -d "scan_$1_3channels" ]; then
+		mkdir "scan_$1_3channels"
+	else
+		if [ ! -z "$(ls -A ./scan_$1_3channels)" ]; then
+			save_Thrash $1
+			mkdir "scan_$1_3channels"
+		fi
+	fi
+}
+
+take_photo(){
+	echo $1
+}
 
 
+
+#################################################################
+## Main
+
+# Managing old folders
+manage_folders "AV"
+manage_folders "PG"
+
+# Acquiring loop
+
+i=0
+fringes="./fringes/*.png"
+for fringe in $fringes
+do
+	# Show pictures
+	eog --single-window -f $fringe &
+	sleep 5
+	
+	# Taking photo
+	i=$(($i+1))
+	take_photo $i
+done
+
+./unwrap.sh
 
