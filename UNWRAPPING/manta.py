537374591216358dcf424c6b092eb6a348e60f63
labo <imflyingtoast@hotmail.com>
2020-03-12 12:47:52 -0400

    photo par python 2 cam

--- UNWRAPPING/manta.py
+++ UNWRAPPING/manta.py
@@ -1,10 +1,95 @@
 import pymba
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+from pymba import Vimba, VimbaException
+from typing import Optional
+from pymba import Frame
 
-from pymba import Vimba
 
-def get_camera_object():
-  with Vimba() as vimba:
-      # provide camera index or id
-      camera = vimba.camera(0)
-      print(camera)
-def 
+def main(name, destination=None):
+    """
+    Prend une seule photo la caméra détectée et l'enregistre sous le nom <image_name>.png en format png dans le répertoire <destination>.
+    name (str):
+        nom pour enregistrer l'image sans l'extension
+    destination (str):
+        path relatif/absolu au répertoire de sauvegarge
+        default=None. Si non spécifié, sauvegarde dans le repertoire actuel.
+    """
+    with Vimba() as vimba:
+        cam = vimba.camera(0)
+        cam.open()
+        cam.arm('SingleFrame')
+
+        print('Starting image capture...')
+        frame=take_frame(cam)
+        cam.disarm()
+        cam.close()
+        
+    #display_frame(frame) 
+    print('Saving image...')
+    save_frame(frame, name, destination)
+
+    input('Done! Press Enter to exit...\n')
+
+
+def get_camera():
+    with Vimba() as vimba:
+        # provide camera index or id
+        cam = vimba.camera(0)
+        return cam
+
+def take_frame(cam):
+
+    # capture a single frame, more than once if desired
+    for i in range(10):
+        try:
+            frame = cam.acquire_frame()
+        except VimbaException as e:
+            # rearm camera upon frame timeout
+            if e.error_code == VimbaException.ERR_TIMEOUT:
+                print(e)
+                cam.disarm()
+                cam.arm('SingleFrame')
+            else:
+                raise
+    return frame
+
+def display_frame(frame, delay=1):
+    """
+    Displays the acquired frame.
+    frame: The frame object to display.
+    delay: Display delay in milliseconds, use 0 for indefinite.
+    """
+    image = frame_to_image(frame)
+
+    # display image
+    cv2.imshow('Image', image)
+    cv2.waitKey(delay)
+
+def save_frame(frame, name, destination=None):
+    """
+    Saves the frame as a png image.
+    frame : The frame object to save 
+    name : The name of the image to save (without .png extension)
+    destination (optional) : the absolute/relative path to the directory where to save the image
+    """
+    
+    # get a copy of the frame data
+    image = frame_to_image(frame)
+    path = "{}{}.png".format( destination if destination is not None else "", name)
+    # save image
+    isSaved = cv2.imwrite(path, image)
+
+def frame_to_image(frame):
+
+    image = frame.buffer_data_numpy()
+    #Converted the datatype to np.uint8
+    new_image = image.astype(np.uint8)
+
+
+    return new_image     
+
+
+main('ma')
+
--- UNWRAPPING/pointGrey.py
+++ UNWRAPPING/pointGrey.py
@@ -4,60 +4,37 @@
 
 import PyCapture2
 
-def take_one_photo(image_name, dir=None, num_images_to_grab=10):
+def main(name, destination=None, num_images_to_grab=10):
 
     """
-    Prend une photo la caméra détectée et l'enregistre sous le nom <image_name>.png en format png dans le répertoire <dir>.
-    image_name (str):
+    Prend une seule photo la caméra détectée et l'enregistre sous le nom <image_name>.png en format png dans le répertoire <destination>.
+    name (str):
         nom pour enregistrer l'image sans l'extension
-    dir (str):
-        path non relatif au répertoire de sauvegarge
-        default=None. Si non spécifié, sauvegarde dans le wd.
+    destination (str):
+        path relatif/absolu au répertoire de sauvegarge
+        default=None. Si non spécifié, sauvegarde dans le repertoire actuel.
     num_images_to_grab (int) :
         nombre de capture de buffer à prendre.
-        default=10.
+        default=1.
     """
 
     # Trouver la camera
-    cam = find_camera()
-    # Enable camera embedded timestamp
-    enable_embedded_timestamp(cam, True)
+    cam = get_camera()
 
     # Take the image
     print('Starting image capture...')
     cam.startCapture()
-    grab_images(cam, image_name, dir, num_images_to_grab)
+    frame = take_frame(cam, num_images_to_grab)
     cam.stopCapture()
-
-    # Disable camera embedded timestamp
-    enable_embedded_timestamp(cam, False)
     cam.disconnect()
+
+    # Save image
+    print('Saving image...')
+    save_frame(frame, name, destination)
 
     input('Done! Press Enter to exit...\n')
 
-
-def grab_images(cam, image_name, dir=None, num_images_to_grab=10):
-    """ Prendre une photo """
-    prev_ts = None
-    for i in range(num_images_to_grab):
-        try:
-            image = cam.retrieveBuffer()
-        except PyCapture2.Fc2error as fc2Err:
-            print('Error retrieving buffer : %s' % fc2Err)
-            continue
-
-        ts = image.getTimeStamp()
-        if prev_ts:
-            diff = (ts.cycleSeconds - prev_ts.cycleSeconds) * 8000 + (ts.cycleCount - prev_ts.cycleCount)
-            print('Timestamp [ %d %d ] - %d' % (ts.cycleSeconds, ts.cycleCount, diff))
-        prev_ts = ts
-
-    save_name = "{}{}".format(dir if dir is not None else "", image_name)
-    newimg = image.convert(PyCapture2.PIXEL_FORMAT.BGR)
-    print('Saving the last image to {}.png'.format(save_name))
-    newimg.save(save_name.encode('utf-8'), PyCapture2.IMAGE_FILE_FORMAT.PNG)
-
-def find_camera():
+def get_camera():
     """ Trouve une camera physique et retourne son object camera """
     # Ensure sufficient cameras are found
     bus = PyCapture2.BusManager()
@@ -72,6 +49,25 @@ def find_camera():
     uid = bus.getCameraFromIndex(0)
     cam.connect(uid)
     return cam
+
+def take_frame(cam, num_images_to_grab=10):
+
+    for i in range(num_images_to_grab):
+        try:
+            frame = cam.retrieveBuffer()
+        except PyCapture2.Fc2error as fc2Err:
+            print('Error retrieving buffer : %s' % fc2Err)
+            continue
+    return frame
+
+
+def save_frame(frame, name, destination=None):
+
+    path = "{}{}.png".format( destination if destination is not None else "", name)
+    newimg = frame.convert(PyCapture2.PIXEL_FORMAT.BGR)
+    isSaved = newimg.save(path.encode('utf-8'), PyCapture2.IMAGE_FILE_FORMAT.PNG)
+    
+
 
 def enable_embedded_timestamp(cam, enable_timestamp):
     """ Permet d'activer/désactiver le compteur interne (timestep) de la camera <cam>."""
