d1f8aaf1e3fbbc8938a780b12d2dcf1a8d78e50a
Merlingot <merlingot1@gmail.com>
2020-03-24 12:00:41 -0400

    centre écran ajout

--- Ecran.py
+++ Ecran.py
@@ -4,27 +4,46 @@ import numpy as np
 
 class Ecran:
 
-    def __init__(self, W, w):
+    def __init__(self, W, w, c):
         """
         W = np.array([Wx,Wy]) # [W]=m
         w = np.array([wx,wy]) # [w]=pixel
+        c = np.array([cu,cv]) # [c]=pixel
         """
         self.W=W
         self.w=w
+        self.c=np.array([c[0],c[1],1])
 
-        alpha_x = W[0]/w[0]; alpha_y = W[1]/w[1]
+        # Facteur de conversion
+        alpha_x = W[0]/w[0]; alpha_y = W[1]/w[1]  # m/pixel
 
-        self.loic = np.zeros((3,3))
-        self.loic[0,0]=alpha_x; self.loic[1,1]=alpha_y
+        # Matrice de passage [u',v',1] -> [X,Y,0]
+        self.M = np.eye((3,3))
+        self.M[0,0]=alpha_x; self.M[1,1]=alpha_y
+        # Matrice de passage [X,Y,0] -> [u',v',1]
+        self.Minv = np.eye((3,3))
+        self.Minv[0,0]=1/alpha_x; self.Minv[1,1]=1/alpha_y
 
     def pixelToSpace(self, vecPix):
         """
-        * homogene
+        * homogene en entrée
         Args:
         vecPix: np.array([u',v',1])
-            Vecteur de position en pixels
+            Vecteur de position en pixels de l'écran
         Returns:
-            np.array([x,y,0])
-            Vecteur de position en m
+            np.array([X,Y,0])
+            Vecteur de position en m sur l'écran
         """
-        return self.loic@vecPix
+        return self.M@(vecPix - self.c)
+
+    def spaceToPixel(self, vecSpace):
+        """
+        * homogene en sortie
+        Args:
+        vecSpace: np.array(X,Y,0])
+            Vecteur de position en m sur l'écran
+        Returns:
+            np.array([u',v',1])
+            Vecteur de position en pixels de l'écran
+        """
+        return self.Minv@vecSpace + self.c
