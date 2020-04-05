97406999c205712c9d58681e468a81fda0f433ce
grolaid2k <gobloic@gmail.com>
2020-04-04 21:24:26 -0400

    loic mat

--- Ecran.py
+++ Ecran.py
@@ -12,38 +12,37 @@ class Ecran:
         """
         self.W=W
         self.w=w
-        self.c=np.array([c[0],c[1],1])
 
         # Facteur de conversion
         alpha_x = W[0]/w[0]; alpha_y = W[1]/w[1]  # m/pixel
+        cu = c[0]; cv = c[1] # pixel
 
-        # Matrice de passage [u',v',1] -> [X,Y,0]
-        self.M = np.eye(3)
-        self.M[0,0]=alpha_x; self.M[1,1]=alpha_y
-        # Matrice de passage [X,Y,0] -> [u',v',1]
-        self.Minv = np.eye(3)
-        self.Minv[0,0]=1/alpha_x; self.Minv[1,1]=1/alpha_y
+        # Matrice de passage [u',v',1] -> [X,Y,0,1]
+        self.M = np.array([[alpha_x,0,alpha_x*cu],[0,alpha_y,alpha_y*cv],[0,0,0],[0,0,1]])
+        # Matrice de passage [X,Y,0,1] -> [u',v',1]
+        self.Minv = np.array([[1/alpha_x,0,0,-cu],[0,1/alpha_y,0,-cv],[0,0,0,1]])
+        
 
     def pixelToSpace(self, vecPix):
         """
-        * homogene en entrée
+        * homogene
         Args:
         vecPix: np.array([u',v',1])
             Vecteur de position en pixels de l'écran
         Returns:
-            np.array([X,Y,0])
+            np.array([X,Y,0,1])
             Vecteur de position en m sur l'écran
         """
-        return self.M@(vecPix - self.c)
+        return self.M@vecPix 
 
     def spaceToPixel(self, vecSpace):
         """
-        * homogene en sortie
+        * homogene
         Args:
-        vecSpace: np.array(X,Y,0])
+        vecSpace: np.array(X,Y,0,1])
             Vecteur de position en m sur l'écran
         Returns:
             np.array([u',v',1])
             Vecteur de position en pixels de l'écran
         """
-        return self.Minv@vecSpace + self.c
+        return self.Minv@vecSpace
